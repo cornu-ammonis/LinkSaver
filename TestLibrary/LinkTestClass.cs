a2109bd79d69eb4fa5c5b7c5b56190e92ebfb2ec
cornu-ammonis <andrewjones232@gmail.com>
2016-11-27 17:14:20 -0500

    restore unit testing

---
+++ TestLibrary/LinkTestClass.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LinkSaver.Models;
+using LinkSaver.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestLibrary
+{
+    [TestFixture]
+    public class LinkTestClass
+    {
+        public LinkTestClass()
+        {
+        }
+
+        public async Task AllLinksRepositoryTest()
+        {
+
+            providerClass provider = new providerClass();
+            ILinkRepository repository = provider.giveRepository();
+            ApplicationDbContext context = provider.giveContext();
+
+            List<Link> repositoryList = await repository.AllLinksToListAsync();
+            List<Link> contextList = await context.Links.Include<Link, Category>(l => l.category).ToListAsync();
+
+            Assert.That(repositoryList, Is.EqualTo(contextList));
+            /*
+            foreach (Link link in repositoryList)
+            {
+                Link contextLink = contextList.Single(l => l.LinkId == link.LinkId);
+                contextLink.category = new Category();
+                contextLink.category.UrlSlug = "notthis";
+                Assert.AreEqual(link, contextLink);
+                Assert.AreEqual(link.category.UrlSlug, contextLink.category.UrlSlug);
+            } */
+        }
+
+        [TestCase]
+        public async Task LinksByCategoryToListAsyncTest()
+        {
+            providerClass provider = new providerClass();
+            ILinkRepository repository = provider.giveRepository();
+            ApplicationDbContext context = provider.giveContext();
+
+            Category testCategory = context.Categories.FirstOrDefault();
+
+            List<Link> testList = await repository.LinksByCategoryToListAsync(testCategory.UrlSlug);
+            /* Link incorrect = context.Links.Include<Link, Category>(l => l.category)
+                 .First(l => l.category.Name == "new");
+             testList.Add(incorrect); */ //code which adds a link of the wrong category to make sure test fails
+            List<string> slugsList = new List<string>();
+
+            foreach (Link link in testList)
+            {
+                slugsList.Add(link.category.UrlSlug);
+            }
+
+            Assert.That(slugsList, Is.All.EqualTo(testCategory.UrlSlug));
+        }
+
+        [TestCase]
+        public void LinksByCategoryToListTest()
+        {
+            providerClass provider = new providerClass();
+            ILinkRepository repository = provider.giveRepository();
+            ApplicationDbContext context = provider.giveContext();
+
+            Category testCategory = context.Categories.FirstOrDefault();
+
+            List<Link> testList = repository.LinksByCategoryToList(testCategory.UrlSlug);
+            List<string> slugsList = new List<string>();
+
+            foreach (Link link in testList)
+            {
+                slugsList.Add(link.category.UrlSlug);
+            }
+
+            Assert.That(slugsList, Is.All.EqualTo(testCategory.UrlSlug));
+
+        }
+
+        [TestCase]
+        public async Task LinksByCategoryAsyncAndSyncAreEquivalent()
+        {
+            providerClass provider = new providerClass();
+            ILinkRepository repository = provider.giveRepository();
+            ApplicationDbContext context = provider.giveContext();
+
+            Category testCategory = context.Categories.FirstOrDefault();
+            List<Link> asyncTestList = await repository.LinksByCategoryToListAsync(testCategory.UrlSlug);
+            List<Link> syncTestList = repository.LinksByCategoryToList(testCategory.UrlSlug);
+            Assert.That(syncTestList, Is.EquivalentTo(asyncTestList));
+
+
+        }
+
+        [TestCase]
+        public async Task AddLinkToDatabaseAsyncTest()
+        {
+            providerClass provider = new providerClass();
+            ILinkRepository repository = provider.giveRepository();
+            ApplicationDbContext context = provider.giveContext();
+            LinkCreationViewModel testCreate = new LinkCreationViewModel();
+            testCreate.url = "yahoo.com";
+            testCreate.category = "unittestcat";
+            await repository.AddLinkToDatabaseAsync(testCreate);
+
+            Assert.That(context.Links.Any(l => l.category.Name == testCreate.category && l.url == testCreate.url));
+            context.Links.Remove(context.Links.Single(l => l.category.Name == testCreate.category &&
+            l.url == testCreate.url));
+            context.SaveChanges();
+        }
+
+
+    }
+}
---
+++ TestLibrary/providerClass.cs
@@ -0,0 +1,39 @@
+﻿using LinkSaver.Data;
+using LinkSaver.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public class providerClass
+    {
+
+        ApplicationDbContext _context;
+        ILinkRepository _repository;
+        public providerClass()
+        {
+            IServiceCollection collection = new ServiceCollection();
+            collection.AddScoped<ILinkRepository, LinkRepository>();
+            collection.AddDbContext<ApplicationDbContext>(options =>
+           options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-LinkSaver-15b46fb3-af8e-414e-82f5-c7bb6e61402a;Trusted_Connection=True;MultipleActiveResultSets=true"));
+            IServiceProvider provider = collection.BuildServiceProvider();
+            _repository = provider.GetService<ILinkRepository>();
+            _context = provider.GetService<ApplicationDbContext>();
+        }
+
+        public ApplicationDbContext giveContext()
+        {
+            return _context;
+        }
+
+        public ILinkRepository giveRepository()
+        {
+            return _repository;
+        }
+
+    }
+}
